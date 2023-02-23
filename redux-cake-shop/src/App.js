c6914ba2bd9f93016a2c42254ec5d3fb2c40fef6
hardikd-zignuts <hardikd@zignuts.com>
2023-02-23 16:22:54 +0530

    Created Action Creator for Cake Shop

---
+++ redux-cake-shop/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CakeShop from './components/CakeShop';
+
+function App() {
+  return (
+    <div className="App">
+      <CakeShop />
+    </div>
+  );
+}
+
+export default App;
---
+++ redux-cake-shop/src/components/CakeShop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CakeShop = () => {
+  return (
+    <>
+    <div>
+        <h1>Number of Cake</h1>
+        <button>Buy Cake</button>
+    </div>
+    </>
+  )
+}
+
+export default CakeShop
